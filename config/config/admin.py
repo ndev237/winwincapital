e55d7707f65ae35e2d0ff2e3c489eea66a815450
ndev237 <ecladorvynil507@gmail.com>
2026-02-21 11:17:26 +0100

    personalisation du tableau de bord

--- config/config/admin.py
+++ config/config/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 # Personnalisation de l'admin
-admin.site.site_header = "Win Win Capital - Administration"
-admin.site.site_title = "Win Win Capital Admin"
+admin.site.site_header = "Win Win Capital"
+admin.site.site_title = "Administration Win Win"
 admin.site.index_title = "Tableau de bord"
+
+# Ajouter le CSS personnalisé
+class CustomAdminSite(admin.AdminSite):
+    class Media:
+        css = {
+            'all': ('admin/css/custom_admin.css',)
+        }
+
+# Utiliser le site personnalisé
+admin.site = CustomAdminSite()  # Décommenter si vous voulez remplacer complètement
